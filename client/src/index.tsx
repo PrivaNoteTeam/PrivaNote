181e010e2b98a52a919d5cfe59ef05c4d10b0396
Olivier Goulet <olivier_goulet@icloud.com>
2021-09-21 02:20:01 -0400

    Setup react with webpack
    This commit creates a hello world react program inside the electron
    app which PrivaNote client will be built upon. This commit also expanded
    upon the webpack configuration file to bundle the react file.
    
    Resolves #26

---
+++ client/src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div>Hello, world!</div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = [
     {
         mode: 'development',
@@ -11,8 +13,30 @@ module.exports = [
             }]
         },
         output: {
-            path: __dirname + '/src',
+            path: __dirname + '/dist',
             filename: 'electron.js'
         }
+    },
+    {
+        mode: 'development',
+        entry: './src/index.tsx',
+        target: 'electron-renderer',
+        devtool: 'source-map',
+        module: {
+            rules: [{
+                test: /\.ts(x?)$/,
+                include: /src/,
+                use: [{ loader: 'ts-loader' }]
+            }]
+        },
+        output: {
+            path: __dirname + '/dist',
+            filename: 'index.js'
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            })
+        ]
     }
 ]
