a4d87afebf87a5453f5f81d8b96023d5e7e4d322
EHSANHAZARI <57685610+EHSANHAZARI@users.noreply.github.com>
2021-09-22 19:42:10 -0400

    sixth

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,20 @@
 import express from "express";
+import cors from "cors";
 
 const main = async () => {
     const app = express();
 
     const HTTP_PORT = 8080;
 
+    app.use(cors);
+
+    app.get('/', (_, __) => {
+        console.log("Root thing");
+    })
+
     app.listen(HTTP_PORT, () => {
         console.log("Listening on port " + HTTP_PORT);
-    })
+    });
 }
 
 main().catch((error) => {
