7c90e9062d031617c192ccb1bb6f08ccbe471b27
EHSANHAZARI <57685610+EHSANHAZARI@users.noreply.github.com>
2021-09-22 19:37:40 -0400

    fifth commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello");
+import express from "express";
+
+const main = async () => {
+    const app = express();
+
+    const HTTP_PORT = 8080;
+
+    app.listen(HTTP_PORT, () => {
+        console.log("Listening on port " + HTTP_PORT);
+    })
+}
+
+main().catch((error) => {
+    console.error(error);
+})
