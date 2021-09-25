a43fe1f9cf65c070d1054e2eb95926e9cbd71dc3
Jason Xie <thejasonxie@gmail.com>
2021-09-25 16:55:24 -0400

    Added husky pre-commit with prettier and git add .

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,18 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
 const main = async () => {
-    const app = express();
+  const app = express();
 
-    const HTTP_PORT = 8080;
+  const HTTP_PORT = 8080;
 
-    app.use(cors);
+  app.use(cors);
 
-    app.listen(HTTP_PORT, () => {
-        console.log("Listening on port " + HTTP_PORT);
-    });
-}
+  app.listen(HTTP_PORT, () => {
+    console.log('Listening on port ' + HTTP_PORT);
+  });
+};
 
 main().catch((error) => {
-    console.error(error);
-})
+  console.error(error);
+});
