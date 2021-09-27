62295875cce278e9b97b3537b5174a69ed2a4b4f
Olivier Goulet <olivier_goulet@icloud.com>
2021-09-26 20:44:10 -0400

    Expand prettier configs for server and client
    This commit adds more configurations to the prettier configs.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import './styles.css';
 
 function App() {
-  return <div className="text-red-500">Hello, world!</div>;
+	return <div className='text-red-500'>Hello, world!</div>;
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
