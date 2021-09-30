af9362d10ea62a0c6aab38a74db3cb695bd7137b
Olivier Goulet <olivier_goulet@icloud.com>
2021-09-30 17:41:29 -0400

    Add file hierarchy traversal
    This commit adds file hierarchy traversal. The user can open directories and list children directories.

--- client/src/components/editorPanel/fileExplorer/Directory.tsx
+++ client/src/components/editorPanel/fileExplorer/Directory.tsx
@@ -1,30 +1,61 @@
 import React, { useState } from 'react';
 import { FileSystemItem } from '../../../types';
 import ChevronRightIcon from '../../../assets/icons/chevron-right.svg';
+import ChevronDownIcon from '../../../assets/icons/chevron-down.svg';
 import FolderIcon from '../../../assets/icons/folder-f.svg';
+import { getFileSystemItems } from '../../../utils/getFileSystemItems';
+import { Note } from './Note';
 
 interface Props {
 	item: FileSystemItem;
 }
 
 export function Directory({ item }: Props) {
-	const [isOpened, _] = useState(false);
+	const [isOpened, setIsOpened] = useState(false);
+
+	let childItems: FileSystemItem[] = isOpened
+		? getFileSystemItems(item.path)
+		: [];
+
+	const handleClick = () => {
+		setIsOpened(!isOpened);
+	};
 
 	return (
-		<div className='flex items-center py-0.5 pl-8 select-none cursor-pointer hover:bg-gray-700'>
-			{isOpened ? (
-				<></>
-			) : (
-				<ChevronRightIcon fill='#9CA3AF' className='absolute -ml-6' />
-			)}
-			<FolderIcon fill='#9CA3AF' className='self-end w-5 mr-1' />
-			<p
-				className={`${
-					isOpened ? 'text-white' : 'text-gray-300'
-				} text-sm`}
+		<div>
+			<div
+				onClick={handleClick}
+				className='flex items-center py-0.5 pl-8 select-none cursor-pointer hover:bg-gray-700'
 			>
-				{item.name}
-			</p>
+				{isOpened ? (
+					<ChevronDownIcon
+						fill='#9CA3AF'
+						className='absolute -ml-6'
+					/>
+				) : (
+					<ChevronRightIcon
+						fill='#9CA3AF'
+						className='absolute -ml-6'
+					/>
+				)}
+				<FolderIcon fill='#9CA3AF' className='self-end w-5 mr-1' />
+				<p
+					className={`${
+						isOpened ? 'text-white' : 'text-gray-300'
+					} text-sm`}
+				>
+					{item.name}
+				</p>
+			</div>
+			<div className='pl-2'>
+				{childItems.map((item) => {
+					return item.type === 'directory' ? (
+						<Directory item={item} />
+					) : (
+						<Note item={item} />
+					);
+				})}
+			</div>
 		</div>
 	);
 }
--- client/src/components/editorPanel/fileExplorer/Note.tsx
+++ client/src/components/editorPanel/fileExplorer/Note.tsx
@@ -1,15 +1,26 @@
 import React, { useState } from 'react';
 import { FileSystemItem } from '../../../types';
+import FileIcon from '../../../assets/icons/file.svg';
 
 interface Props {
 	item: FileSystemItem;
 }
 
 export function Note({ item }: Props) {
-	const [isOpened, _] = useState(false);
+	const [isOpened, setIsOpened] = useState(false);
+
+	const handleClick = () => {
+		setIsOpened(true);
+	};
 
 	return (
-		<div className='flex select-none cursor-pointer py-0.5 pl-8 hover:bg-gray-700'>
+		<div
+			onClick={handleClick}
+			className={`flex select-none cursor-pointer py-0.5 pl-8 hover:bg-gray-700 ${
+				isOpened && 'bg-gray-700'
+			}`}
+		>
+			<FileIcon fill='#9CA3AF' className='self-end w-5 mr-1' />
 			<p
 				className={`${
 					isOpened ? 'text-white' : 'text-gray-300'
