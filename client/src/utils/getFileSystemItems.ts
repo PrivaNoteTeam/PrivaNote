0ca496a7b1fffcc33e9922bea85c68cb5cd83cad
Olivier Goulet <olivier_goulet@icloud.com>
2021-09-30 20:09:46 -0400

    Add sort to file hierarchy
    This commit sorts the files and directories in alphabetical order.

--- client/src/utils/getFileSystemItems.ts
+++ client/src/utils/getFileSystemItems.ts
@@ -23,5 +23,11 @@ export const getFileSystemItems = (path: string): FileSystemItem[] => {
 				path: `${path}/${name}`,
 				type: data.isDirectory() ? 'directory' : 'note'
 			};
-		});
+		})
+		.sort((a, b) => {
+			if (a.type === b.type)
+				return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1;
+
+			return a.type > b.type ? 1 : -1;
+		}) as FileSystemItem[];
 };
