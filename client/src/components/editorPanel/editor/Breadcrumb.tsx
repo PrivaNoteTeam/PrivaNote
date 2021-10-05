0400938a38d93e15bffa357baf3711485ac9cffa
Olivier Goulet <olivier_goulet@icloud.com>
2021-10-05 05:03:01 -0400

    Add documentation to useRelativePath

--- client/src/components/editorPanel/editor/Breadcrumb.tsx
+++ client/src/components/editorPanel/editor/Breadcrumb.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FileItem } from '../../../types';
+import ChevronIcon from '../../../assets/icons/chevron-right.svg';
 
 interface Props {
 	currentFile: FileItem;
@@ -7,11 +8,24 @@ interface Props {
 }
 
 export function Breadcrumb({ currentFile, unSaved }: Props) {
-	return (
-		<div className='bg-gray-900 pn-drop-shadow'>
-			<p className='text-gray-400 py-1 px-4'>
-				{unSaved ? currentFile.name.concat('*') : currentFile.name}
-			</p>
-		</div>
-	);
+	const parts = currentFile.path.split(/[\/\\]/);
+
+	const render = parts.map((part, i) => {
+		let text = part;
+
+		if (i === parts.length - 1) {
+			text = unSaved ? currentFile.name.concat('*') : currentFile.name;
+		} //py-1 px-4
+
+		return (
+			<div className='flex'>
+				{i !== 0 && (
+					<ChevronIcon fill='#9CA3AF' className='self-end w-5 mr-1' />
+				)}
+				<p className='text-gray-400'>{text}</p>
+			</div>
+		);
+	});
+
+	return <div className='bg-gray-900 pn-drop-shadow flex'>{render}</div>;
 }
