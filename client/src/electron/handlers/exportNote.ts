791cd7dd4d02a5cfce19f2d38b72933260243208
Jason Xie <thejasonxie@gmail.com>
2021-10-05 10:34:19 -0400

    Implemented exportNote handler with temporary solution

--- client/src/electron/handlers/exportNote.ts
+++ client/src/electron/handlers/exportNote.ts
@@ -1,7 +1,25 @@
-import { BrowserWindow } from 'electron';
+import { BrowserWindow, dialog } from 'electron';
 import { FileItem } from '../../types';
+import fs from 'fs';
 
 export function exportNote(window: BrowserWindow, currentFile: FileItem) {
-	console.log('exportNote Handler: ', currentFile);
-	console.log(window);
+	dialog
+		.showSaveDialog(window, {
+			defaultPath: currentFile.name.split('.')[0],
+			filters: [
+				{ name: 'PDF', extensions: ['.pdf'] },
+				{ name: 'HTML', extensions: ['.html'] }
+			]
+		})
+		.then((result) => {
+			// TEMPORARY EXPORT, will be fixed in Markdown Parsing milestone
+			// FIX: MUST PARSE MARKDOWN TO HTML THEN PDF
+			if (result.canceled || !result.filePath) return;
+			fs.copyFile(currentFile.path, result.filePath, (err) => {
+				if (err) {
+					alert(currentFile + ' could not be exported.');
+					return;
+				}
+			});
+		});
 }
