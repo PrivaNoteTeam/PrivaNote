5e84993e76c8797e09ef69f3cb3a404c6a81ee14
Olivier Goulet <olivier_goulet@icloud.com>
2021-10-07 11:24:40 -0400

    Separate Directory

--- client/src/components/editorPanel/fileExplorer/Directory.tsx
+++ client/src/components/editorPanel/fileExplorer/Directory.tsx
@@ -1,13 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FileSystemItem, EditorAction } from '../../../types';
-import ChevronRightIcon from '../../../assets/icons/chevron-right.svg';
-import ChevronDownIcon from '../../../assets/icons/chevron-down.svg';
-import FolderIcon from '../../../assets/icons/folder-f.svg';
-import { getFileSystemItems } from '../../../utils/getFileSystemItems';
-import { Note } from './Note';
-import { ipcRenderer } from 'electron';
-import { renameExplorerItem } from '../../../utils/renameExplorerItem';
-import { useStore } from '../../../useStore';
+import { UIDirectory } from './directory/UIDirectory';
+import { useDirectory } from './directory/useDirectory';
 
 interface Props {
 	item: FileSystemItem;
@@ -30,182 +24,42 @@ export function Directory({
 	renameText,
 	setRenameText
 }: Props) {
-	const [{ currentNote }, dispatch] = useStore();
-	const [isOpened, setIsOpened] = useState(false);
-
-	let childItems: FileSystemItem[] = isOpened
-		? getFileSystemItems(item.path)
-		: [];
-
-	const handleClick = () => {
-		if (secondarySelection?.path === item.path) {
-			return;
-		} else if (secondarySelection) {
-			editorDispatch({
-				type: 'secondarySelect',
-				secondarySelection: undefined,
-				isRenaming
-			});
-		}
-
-		editorDispatch({
-			type: 'primarySelect',
-			primarySelection: item,
-			isRenaming
-		});
-
-		setIsOpened(!isOpened);
-	};
-
-	const handleContextMenu = () => {
-		ipcRenderer.send('openExplorerFileContextMenu');
-
-		editorDispatch({
-			type: 'secondarySelect',
-			secondarySelection: item,
-			isRenaming
-		});
-
-		setRenameText(item.name);
-	};
-
-	const handleRenameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setRenameText(event.target.value);
-	};
-
-	const handleRenameKeyDown = (
-		event: React.KeyboardEvent<HTMLInputElement>
-	) => {
-		if (event.key === 'Enter' || event.code === '13') {
-			renameExplorerItem(item.path, renameText)
-				.then((renamedItem) => {
-					editorDispatch({
-						type: 'rename',
-						isRenaming: false
-					});
-
-					editorDispatch({
-						type: 'secondarySelect',
-						secondarySelection: undefined,
-						isRenaming // might have old value
-					});
-
-					if (item.path == currentNote?.path) {
-						dispatch({
-							type: 'openNote',
-							currentNote: renamedItem
-						});
-					}
-				})
-				.catch((error) => {
-					console.log(error);
-				});
-		}
-		if (event.key === 'Escape' || event.code === '27') {
-			editorDispatch({
-				type: 'rename',
-				isRenaming: false
-			});
-
-			editorDispatch({
-				type: 'secondarySelect',
-				secondarySelection: undefined,
-				isRenaming
-			});
-		}
-	};
-
-	const handleRenameBlur = () => {
-		editorDispatch({
-			type: 'rename',
-			isRenaming: false
-		});
-	};
-
-	const handleRenameFocus = (event: React.FocusEvent<HTMLInputElement>) => {
-		event.target.select();
-	};
-
-	let style = '';
-
-	if (secondarySelection?.path === item.path) {
-		style = 'border-blue-500 border-opacity-70 border-2';
-	} else if (primarySelection?.path === item.path) {
-		style =
-			'bg-blue-500 border-blue-300 border bg-opacity-30 border-opacity-30';
-	} else {
-		style =
-			'hover:bg-opacity-30 hover:bg-gray-700 border border-transparent';
-	}
-
-	let displayItem = undefined;
-	if (isRenaming && secondarySelection?.path === item.path) {
-		displayItem = (
-			<input
-				type='text'
-				value={renameText}
-				onChange={handleRenameChange}
-				onKeyDown={handleRenameKeyDown}
-				onBlur={handleRenameBlur}
-				onFocus={handleRenameFocus}
-				autoFocus
-				className='bg-transparent outline-none text-white text-sm'
-			/>
-		);
-	} else {
-		displayItem = <p className='text-gray-300 text-sm'>{item.name}</p>;
-	}
+	const {
+		isOpened,
+		childItems,
+		handleClick,
+		handleContextMenu,
+		handleRenameKeyDown,
+		handleRenameFocus,
+		handleRenameChange,
+		handleRenameBlur
+	} = useDirectory({
+		item,
+		secondarySelection,
+		isRenaming,
+		renameText,
+		setRenameText,
+		editorDispatch
+	});
 
 	return (
-		<div>
-			<div
-				onClick={handleClick}
-				onContextMenu={handleContextMenu}
-				className={`flex items-center py-0.5 select-none cursor-pointer align-bottom ${style}`}
-				style={{ paddingLeft: `${depth + 2}rem` }}
-			>
-				{isOpened ? (
-					<ChevronDownIcon fill='#9CA3AF' className='-ml-6' />
-				) : (
-					<ChevronRightIcon fill='#9CA3AF' className='-ml-6' />
-				)}
-				<FolderIcon
-					fill='#9CA3AF'
-					className='self-end w-5 mr-1 align-bottom'
-				/>
-				{displayItem}
-			</div>
-			<div className='relative'>
-				<div
-					className='absolute h-full bg-gray-600'
-					style={{ marginLeft: `${depth + 1.25}rem`, width: '1px' }}
-				></div>
-				{childItems.map((item) => {
-					return item.type === 'directory' ? (
-						<Directory
-							item={item}
-							depth={depth + 1}
-							primarySelection={primarySelection}
-							secondarySelection={secondarySelection}
-							isRenaming={isRenaming}
-							editorDispatch={editorDispatch}
-							renameText={renameText}
-							setRenameText={setRenameText}
-						/>
-					) : (
-						<Note
-							item={item}
-							depth={depth + 1}
-							primarySelection={primarySelection}
-							secondarySelection={secondarySelection}
-							isRenaming={isRenaming}
-							editorDispatch={editorDispatch}
-							renameText={renameText}
-							setRenameText={setRenameText}
-						/>
-					);
-				})}
-			</div>
-		</div>
+		<UIDirectory
+			item={item}
+			depth={depth}
+			isOpened={isOpened}
+			primarySelection={primarySelection}
+			secondarySelection={secondarySelection}
+			isRenaming={isRenaming}
+			editorDispatch={editorDispatch}
+			renameText={renameText}
+			childItems={childItems}
+			setRenameText={setRenameText}
+			handleClick={handleClick}
+			handleContextMenu={handleContextMenu}
+			handleRenameBlur={handleRenameBlur}
+			handleRenameChange={handleRenameChange}
+			handleRenameFocus={handleRenameFocus}
+			handleRenameKeyDown={handleRenameKeyDown}
+		/>
 	);
 }
