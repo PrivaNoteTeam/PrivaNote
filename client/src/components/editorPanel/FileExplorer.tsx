39c8b58304c795d77529df6414a37e6e20e0d935
Olivier Goulet <olivier_goulet@icloud.com>
2021-10-07 10:36:26 -0400

    Separate FileExplorer

--- client/src/components/editorPanel/FileExplorer.tsx
+++ client/src/components/editorPanel/FileExplorer.tsx
@@ -1,15 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FileSystemItem, EditorAction } from '../../types';
-import { Directory } from './fileExplorer/Directory';
-import { Note } from './fileExplorer/Note';
-import PlusIcon from '../../assets/icons/plus.svg';
-import FolderOpenIcon from '../../assets/icons/folder-open.svg';
-import { getFileName } from '../../utils/getFileName';
-import { createFile } from '../../utils/createFile';
-import { createDirectory } from '../../utils/createDirectory';
-import { getParentDirectory } from '../../utils/getParentDirectory';
-import { renameExplorerItem } from '../../utils/renameExplorerItem';
-import { useStore } from '../../useStore';
+import { UIFileExplorer } from './fileExplorer/UIFileExplorer';
+
+import { useFileExplorer } from './fileExplorer/useFileExplorer';
 
 interface Props {
 	items: FileSystemItem[];
@@ -26,123 +19,31 @@ export function FileExplorer({
 	isRenaming,
 	editorDispatch
 }: Props) {
-	const [{ notebook, currentNote }, dispatch] = useStore();
-	const [renameText, setRenameText] = useState('');
-
-	const handleAddFileClick = () => {
-		const newFilePath = primarySelection
-			? getParentDirectory(primarySelection.path, { onlyFiles: true })
-			: notebook;
-		const newFile = createFile(newFilePath as string);
-
-		dispatch({
-			type: 'openNote',
-			currentNote: newFile
-		});
-	};
-
-	const handleAddDirectoryClick = () => {
-		const newDirectoryPath = primarySelection
-			? getParentDirectory(primarySelection.path, { onlyFiles: true })
-			: notebook;
-		const newDirectory = createDirectory(newDirectoryPath as string);
-
-		editorDispatch({
-			type: 'primarySelect',
-			primarySelection: newDirectory,
-			isRenaming
-		});
-	};
-
-	const handleOuterClick = (event: React.MouseEvent<HTMLDivElement>) => {
-		if (event.target !== event.currentTarget) return;
-
-		editorDispatch({
-			type: 'primarySelect',
-			primarySelection: undefined,
-			isRenaming
-		});
-
-		editorDispatch({
-			type: 'secondarySelect',
-			secondarySelection: undefined,
-			isRenaming
-		});
-
-		if (isRenaming) {
-			renameExplorerItem(secondarySelection?.path!, renameText)
-				.then((renamedItem) => {
-					editorDispatch({
-						type: 'rename',
-						isRenaming: false
-					});
-
-					editorDispatch({
-						type: 'secondarySelect',
-						secondarySelection: undefined,
-						isRenaming
-					});
-
-					if (secondarySelection?.path == currentNote?.path) {
-						dispatch({
-							type: 'openNote',
-							currentNote: renamedItem
-						});
-					}
-				})
-				.catch((error) => {
-					console.log(error);
-				});
-		}
-	};
+	const {
+		handleAddDirectoryClick,
+		handleAddFileClick,
+		handleOuterClick,
+		renameText,
+		setRenameText
+	} = useFileExplorer({
+		primarySelection,
+		secondarySelection,
+		isRenaming,
+		editorDispatch
+	});
 
 	return (
-		<div className='bg-gray-800 pt-2 flex flex-col resize-x'>
-			<div className='flex justify-between'>
-				<p className='text-gray-500 text-sm font-bold px-3 py-1'>
-					{getFileName(notebook as string)}
-				</p>
-				<div className='mx-2 flex space-x-1'>
-					<PlusIcon
-						onClick={handleAddFileClick}
-						fill='#9CA3AF'
-						className='p-0.5 hover:bg-gray-700 rounded-md cursor-pointer'
-					/>
-					<FolderOpenIcon
-						onClick={handleAddDirectoryClick}
-						fill='#9CA3AF'
-						className='p-0.5 hover:bg-gray-700 rounded-md cursor-pointer'
-					/>
-				</div>
-			</div>
-			<div
-				onClick={handleOuterClick}
-				className='flex-grow overflow-y-scroll pb-6'
-			>
-				{items.map((item) => {
-					return item.type === 'directory' ? (
-						<Directory
-							item={item}
-							primarySelection={primarySelection}
-							secondarySelection={secondarySelection}
-							isRenaming={isRenaming}
-							editorDispatch={editorDispatch}
-							renameText={renameText}
-							setRenameText={setRenameText}
-						/>
-					) : (
-						<Note
-							item={item}
-							primarySelection={primarySelection}
-							secondarySelection={secondarySelection}
-							isRenaming={isRenaming}
-							editorDispatch={editorDispatch}
-							renameText={renameText}
-							setRenameText={setRenameText}
-						/>
-					);
-				})}
-			</div>
-		</div>
+		<UIFileExplorer
+			items={items}
+			handleAddDirectoryClick={handleAddDirectoryClick}
+			handleAddFileClick={handleAddFileClick}
+			handleOuterClick={handleOuterClick}
+			renameText={renameText}
+			setRenameText={setRenameText}
+			editorDispatch={editorDispatch}
+			isRenaming={isRenaming}
+			primarySelection={primarySelection}
+			secondarySelection={secondarySelection}
+		/>
 	);
 }
---
+++ client/src/components/editorPanel/fileExplorer/useFileExplorer.ts
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import { useStore } from '../../../useStore';
+import { FileSystemItem, EditorAction } from '../../../types';
+import { createFile } from '../../../utils/createFile';
+import { getParentDirectory } from '../../../utils/getParentDirectory';
+import { createDirectory } from '../../../utils/createDirectory';
+import { renameExplorerItem } from '../../../utils/renameExplorerItem';
+
+interface Args {
+	primarySelection?: FileSystemItem;
+	secondarySelection?: FileSystemItem;
+	editorDispatch: React.Dispatch<EditorAction>;
+	isRenaming: boolean;
+}
+
+export function useFileExplorer({
+	primarySelection,
+	secondarySelection,
+	editorDispatch,
+	isRenaming
+}: Args) {
+	const [{ notebook, currentNote }, dispatch] = useStore();
+	const [renameText, setRenameText] = useState('');
+
+	const handleAddFileClick = () => {
+		const newFilePath = primarySelection
+			? getParentDirectory(primarySelection.path, { onlyFiles: true })
+			: notebook;
+		const newFile = createFile(newFilePath as string);
+
+		dispatch({
+			type: 'openNote',
+			currentNote: newFile
+		});
+	};
+
+	const handleAddDirectoryClick = () => {
+		const newDirectoryPath = primarySelection
+			? getParentDirectory(primarySelection.path, { onlyFiles: true })
+			: notebook;
+		const newDirectory = createDirectory(newDirectoryPath as string);
+
+		editorDispatch({
+			type: 'primarySelect',
+			primarySelection: newDirectory,
+			isRenaming
+		});
+	};
+
+	const handleOuterClick = (event: React.MouseEvent<HTMLDivElement>) => {
+		if (event.target !== event.currentTarget) return;
+
+		editorDispatch({
+			type: 'primarySelect',
+			primarySelection: undefined,
+			isRenaming
+		});
+
+		editorDispatch({
+			type: 'secondarySelect',
+			secondarySelection: undefined,
+			isRenaming
+		});
+
+		if (isRenaming) {
+			renameExplorerItem(secondarySelection?.path!, renameText)
+				.then((renamedItem) => {
+					editorDispatch({
+						type: 'rename',
+						isRenaming: false
+					});
+
+					editorDispatch({
+						type: 'secondarySelect',
+						secondarySelection: undefined,
+						isRenaming
+					});
+
+					if (secondarySelection?.path == currentNote?.path) {
+						dispatch({
+							type: 'openNote',
+							currentNote: renamedItem
+						});
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		}
+	};
+
+	return {
+		handleAddFileClick,
+		handleAddDirectoryClick,
+		handleOuterClick,
+		renameText,
+		setRenameText
+	};
+}
