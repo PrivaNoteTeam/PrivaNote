ff891fb4222b0e5c9ab68a1938ce2ac64397c3aa
Jason Xie <thejasonxie@gmail.com>
2021-10-08 14:57:19 -0400

    Added sessions with connectPgSimple

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,30 @@
 import express from 'express';
 import cors from 'cors';
+import pg, { Connection } from 'pg';
+import session from 'express-session';
+import connectPgSimple from 'connect-pg-simple';
 
 const main = async () => {
 	const app = express();
 
 	const HTTP_PORT = 8080;
 
+	const pgPool = new pg.Pool({
+		// Insert pool options here
+	});
+
 	app.use(cors());
+	app.use(
+		session({
+			store: new (connectPgSimple(session))({
+				pool: pgPool,
+				tableName: 'session'
+			}),
+			secret: 'secret key',
+			resave: false,
+			cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
+		})
+	);
 
 	app.get('/', (_, res, __) => {
 		res.send(`<h1>Hello from PrivaNote Server`);
