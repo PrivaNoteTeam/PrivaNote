40fe61d798c4dab3a36d959cfba98d9e833bee2d
Jason Xie <thejasonxie@gmail.com>
2021-10-16 19:03:52 -0400

    Updated LoginModal to display form error

--- client/src/app/components/modalManager/LoginModal.tsx
+++ client/src/app/components/modalManager/LoginModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { ModalLayout } from './Modal';
 import { TextField } from '../TextField';
@@ -7,6 +7,9 @@ import { LoginFormValues } from '@types';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { loginUser } from '@shared/api/loginUser';
+import { FormBanner } from './FormBanner';
+import { FormError } from '@types';
+import { AxiosError } from 'axios';
 
 const validationSchema = yup.object({
 	email: yup.string().email('Invalid email').required(),
@@ -15,12 +18,13 @@ const validationSchema = yup.object({
 
 export function LoginModal() {
 	const [, modalManagerDispatch] = useModalStore();
+	const [formError, setFormError] = useState<FormError | undefined>();
 
 	const {
 		register,
 		formState: { errors },
-		handleSubmit: useFormHandleSubmit
-		// setError
+		handleSubmit: useFormHandleSubmit,
+		setError
 	} = useForm<LoginFormValues>({
 		mode: 'onBlur',
 		resolver: yupResolver(validationSchema)
@@ -36,14 +40,38 @@ export function LoginModal() {
 
 	const submitHandler = useFormHandleSubmit(
 		async ({ email, password }: LoginFormValues) => {
-			const response = await loginUser({ email, password });
+			const unknownError = { message: 'An unknown error has occurred' };
 
-			if (response.user) {
-				modalManagerDispatch({
-					type: 'verificationModal',
-					verificationModalVisible: true
+			loginUser({ email, password })
+				.then((response) => {
+					if (response.fieldError) {
+						if (
+							response.fieldError.field !== 'email' &&
+							response.fieldError.field !== 'password'
+						)
+							return;
+						setError(response.fieldError.field!, {
+							message: response.fieldError.message
+						});
+					} else if (response.user) {
+						modalManagerDispatch({
+							type: 'verificationModal',
+							verificationModalVisible: true
+						});
+					} else if (response.formError) {
+						setFormError(response.formError);
+					} else {
+						setFormError(unknownError);
+					}
+				})
+				.catch((err: AxiosError) => {
+					setFormError({ message: err.message });
+				})
+				.catch(() => {
+					setFormError(unknownError);
 				});
-			}
+
+			return;
 		}
 	);
 
@@ -61,6 +89,9 @@ export function LoginModal() {
 					<h2 className='text-center text-2xl text-white select-none'>
 						Sign in to your account
 					</h2>
+					{formError && (
+						<FormBanner text={formError.message} status='error' />
+					)}
 					<p className='text-gray-500 text-xs break-words text-center select-none'>
 						Signing into a PrivaNote account allows you to{' '}
 						<span className='text-gray-400'>
