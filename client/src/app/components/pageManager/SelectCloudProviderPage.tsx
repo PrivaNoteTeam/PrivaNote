5305a6ac35f4622796886999f47e5e1552697f63
Olivier Goulet <olivier_goulet@icloud.com>
2021-10-22 08:56:23 -0400

    Add back button to select cloud provider page

--- client/src/app/components/pageManager/SelectCloudProviderPage.tsx
+++ client/src/app/components/pageManager/SelectCloudProviderPage.tsx
@@ -19,11 +19,29 @@ export function SelectCloudProviderPage() {
 					<h1 className='text-2xl text-white select-none'>
 						Select a cloud storage provider
 					</h1>
+					<p className='text-gray-500'>
+						Choose from a variety of supported cloud providers,
+						including our{' '}
+						<span className='text-gray-400'>PrivaNote Vault</span>.
+					</p>
 				</div>
-				<div role='listbox' className='flex flex-col space-y-2'>
+				<div role='listbox' className='flex flex-col space-y-4'>
 					<ProviderItem provider='PrivaNote Vault' />
 					<ProviderItem provider='Google Drive' />
 					<ProviderItem provider='OneDrive' />
+				</div>
+				<div className='flex justify-end'>
+					<button
+						onClick={() => {
+							pageDispatch({
+								type: 'cloudProviderPage',
+								cloudProviderPageVisible: true
+							});
+						}}
+						className='pn-button bg-gray-500 bg-opacity-50 border-gray-500 hover:border-gray-400'
+					>
+						Go back
+					</button>
 				</div>
 			</div>
 		</Page>
