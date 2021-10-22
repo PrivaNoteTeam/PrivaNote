822d01b3193507bde061da70a40b1a5f574b8e77
Olivier Goulet <olivier_goulet@icloud.com>
2021-10-22 09:25:23 -0400

    Add more style to select cloud provider page

--- client/src/app/components/pageManager/cloudProviderPage/providerItem/UIProviderItem.tsx
+++ client/src/app/components/pageManager/cloudProviderPage/providerItem/UIProviderItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ShieldCheck from '@assets/icons/shield-check-f.svg';
 
 interface Props {
 	provider: 'Google Drive' | 'OneDrive' | 'PrivaNote Vault';
@@ -9,7 +10,7 @@ export function UIProviderItem({ logo, provider }: Props) {
 	return (
 		<div className='flex justify-between border border-gray-700 rounded-md p-4'>
 			<div className='flex space-x-4 align-baseline'>
-				<div className='h-16'>
+				<div className='h-16 flex-shrink-0'>
 					<img
 						src={logo}
 						className='w-full h-full select-none'
@@ -18,10 +19,33 @@ export function UIProviderItem({ logo, provider }: Props) {
 					/>
 				</div>
 				<div className='flex flex-col align-middle justify-center'>
-					<header className='text-white text-lg'>{provider}</header>
+					<div className='flex space-x-2'>
+						<header className='text-white text-lg'>
+							{provider}
+						</header>
+						{provider === 'PrivaNote Vault' && (
+							<ShieldCheck className='text-green-400' />
+						)}
+					</div>
 					<p className='text-xs text-gray-400'>
 						Last synced 5 minutes ago
 					</p>
+
+					{provider === 'PrivaNote Vault' && (
+						<div className='text-gray-500 text-sm overflow-ellipsis mt-2'>
+							<header className='text-white text-md'>
+								Why you should choose PrivaNote Vault
+							</header>
+							<p>
+								Lorem ipsum dolor sit amet consectetur
+								adipisicing elit. Voluptate fuga maiores nostrum
+								consectetur tenetur debitis neque numquam,
+								obcaecati facilis? Veritatis consectetur aliquam
+								debitis doloremque consequuntur voluptate ut
+								nulla error deserunt.
+							</p>
+						</div>
+					)}
 				</div>
 			</div>
 			<div role='list' className='flex flex-col space-y-3'>
