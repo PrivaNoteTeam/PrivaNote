3a712ef67c47868c0aa725284438f351feefff8d
Olivier Goulet <olivier_goulet@icloud.com>
2021-10-26 15:27:58 -0400

    Add ordered lists and code blocks

--- client/src/app/components/editorPanel/preview/usePreview.ts
+++ client/src/app/components/editorPanel/preview/usePreview.ts
@@ -1,8 +1,13 @@
 import marked from 'marked';
 import parse from 'html-react-parser';
+import hljs from 'highlight.js';
 
 export function usePreview(text: string) {
-	const html = marked(text);
+	const html = marked(text, {
+		highlight: (code: string) => {
+			return hljs.highlightAuto(code).value; // no lang selected ,throws error
+		}
+	});
 
 	return parse(html);
 }
