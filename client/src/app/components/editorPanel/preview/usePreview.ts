3adf7c45f9949ce1a128db23a3b731f86809cafe
Olivier Goulet <olivier_goulet@icloud.com>
2021-10-28 18:56:05 -0400

    Relative paths for images

--- client/src/app/components/editorPanel/preview/usePreview.ts
+++ client/src/app/components/editorPanel/preview/usePreview.ts
@@ -1,12 +1,16 @@
 import marked from 'marked';
 import parse from 'html-react-parser';
 import hljs from 'highlight.js';
+import { useStore } from '@hooks';
 
 export function usePreview(text: string) {
+	const [{ notebook }] = useStore();
+
 	const html = marked(text, {
 		highlight: (code: string) => {
 			return hljs.highlightAuto(code).value; // no lang selected ,throws error
-		}
+		},
+		baseUrl: notebook
 	});
 
 	return parse(html);
