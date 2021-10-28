aeffabb72290be4557d06b6022229701e07f7170
Olivier Goulet <olivier_goulet@icloud.com>
2021-10-28 11:17:14 -0400

    Add icon styles

--- client/src/app/components/fileExplorer/Node.tsx
+++ client/src/app/components/fileExplorer/Node.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { useNode } from './node/useNode';
 import { FileSystemItem } from '@types';
 import DirectoryIcon from '@assets/icons/folder-f.svg';
@@ -14,16 +14,20 @@ export function Node({ item }: Props) {
 	const {} = useNode({ item });
 
 	let icon: JSX.Element;
+	const iconStyle = {
+		fill: '#9CA3AF',
+		className: 'self-end w-5 mr-1 align-bottom'
+	};
 
 	switch (item.type) {
 		case 'directory':
-			icon = <DirectoryIcon />;
+			icon = <DirectoryIcon {...iconStyle} />;
 			break;
 		case 'note':
-			icon = <NoteIcon />;
+			icon = <NoteIcon {...iconStyle} />;
 			break;
 		case 'attachment':
-			icon = <AttachmentIcon />;
+			icon = <AttachmentIcon {...iconStyle} />;
 			break;
 	}
 
