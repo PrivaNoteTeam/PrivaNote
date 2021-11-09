2ed1fe63176e44a0b810c39e5ef3d6a04d75494f
Jason Xie <thejasonxie@gmail.com>
2021-11-08 19:04:20 -0500

    Implemented uploadEntireNotebook when privanote folder exists in drive

--- client/src/shared/Api/googleDrive/initializeGoogleDrive.ts
+++ client/src/shared/Api/googleDrive/initializeGoogleDrive.ts
@@ -1,16 +1,17 @@
-import fs from 'fs';
+// import fs from 'fs';
 import { searchForFolder } from './searchForFolder';
 import { uploadEntireNotebook } from './uploadEntireNotebook';
 import { createAFolder } from './createAFolder';
-import { getNotebookLocation } from '@shared/notebook';
+import { getNotebookLocation, getNotebookName } from '@shared/notebook';
 import { getNotebookStructure } from '@shared/utils/synchronization/getNotebookStructure';
 
 let ROOT_DRIVE_FOLDER_NAME = 'privanote';
 let ROOT_DRIVE_FOLDER_ID = '';
 
 export const initializeGoogleDrive = () => {
+	let notebookName = getNotebookName();
 	let notebookLocation = getNotebookLocation();
-	let notebookItems = getNotebookStructure(notebookLocation);
+	let notebookStructure = getNotebookStructure(notebookLocation);
 
 	searchForFolder(ROOT_DRIVE_FOLDER_NAME).then(async (folders) => {
 		if (!folders || !folders.length) {
@@ -19,18 +20,26 @@ export const initializeGoogleDrive = () => {
 			await createAFolder({ name: ROOT_DRIVE_FOLDER_NAME })
 				.then(async (res) => {
 					ROOT_DRIVE_FOLDER_ID = res.id;
-					notebookItems = await uploadEntireNotebook(
-						notebookItems,
+					await uploadEntireNotebook(
+						notebookStructure,
 						ROOT_DRIVE_FOLDER_ID
 					);
 				})
 				.catch((err) => console.log(err));
-
-			fs.writeFileSync(
-				`${notebookLocation}/.privanote/notebookStructure.json`,
-				JSON.stringify(notebookItems, null, 4)
-			);
 		} else {
+			ROOT_DRIVE_FOLDER_ID = folders[0].id!;
+			searchForFolder(notebookName, ROOT_DRIVE_FOLDER_ID)
+				.then(async (folders) => {
+					if (!folders || !folders.length) {
+						await uploadEntireNotebook(
+							notebookStructure,
+							ROOT_DRIVE_FOLDER_ID
+						);
+					} else {
+						// check if matches notebook id, if not create new
+					}
+				})
+				.catch((err) => console.log(err));
 			// synchronize google drive with current files
 			// createAFolder(ROOT_DRIVE_FOLDER_NAME, nanoid())
 			// 	.then((res) => console.log(res))
--- client/src/shared/Api/googleDrive/searchForFolder.ts
+++ client/src/shared/Api/googleDrive/searchForFolder.ts
@@ -1,7 +1,12 @@
 import { getDrive } from './setup';
 
-export const searchForFolder = async (name: string) => {
+export const searchForFolder = async (name: string, parentID: string = '') => {
 	try {
+		let query = `name = '${name}' and mimeType = 'application/vnd.google-apps.folder' and trashed=false`;
+		if (parentID) {
+			query = `${query} '${parentID}' in parents`;
+		}
+
 		const res = await getDrive().files.list({
 			q: `name = '${name}' and mimeType = 'application/vnd.google-apps.folder' and trashed=false`,
 			fields: 'nextPageToken, files(id, name)',
