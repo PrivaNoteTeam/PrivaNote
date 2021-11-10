53ab284242657d2d8cf67b11921606c90bd34959
Jason Xie <thejasonxie@gmail.com>
2021-11-10 06:46:33 -0500

    Fixed the launching error in Windows OS when opening App from browser

--- client/src/electron/main.ts
+++ client/src/electron/main.ts
@@ -7,13 +7,14 @@ import installExtension, {
 } from 'electron-devtools-installer';
 import { getToken, setGoogleAuth } from '@shared/Api/googleDrive/setup';
 import { initializeGoogleDrive } from '@shared/Api/googleDrive/initializeGoogleDrive';
+import path from 'path';
 
 const handleReady = () => {
 	registerIpcHandlers();
 	createMainWindow();
 };
 
-app.requestSingleInstanceLock();
+const gotTheLock = app.requestSingleInstanceLock();
 
 app.on('ready', handleReady);
 
@@ -27,49 +28,72 @@ app.whenReady().then(() => {
 
 let deepLinkingUrl: any;
 
-if (process.platform === 'win32') {
-	deepLinkingUrl = process.argv.slice(1);
+if (process.defaultApp) {
+	if (process.argv.length >= 2) {
+		app.setAsDefaultProtocolClient('privanote', process.execPath, [
+			path.resolve(process.argv[1])
+		]);
+	}
+} else {
+	app.setAsDefaultProtocolClient('privanote');
 }
 
-if (!app.isDefaultProtocolClient('privanote')) {
-	app.setAsDefaultProtocolClient('privanote');
+const handleOpenURL = async (url: string) => {
+	const urlObject = new URL(url);
+	const mainWindow = getMainWindow();
+
+	if (url.startsWith('privanote://reset-password')) {
+		mainWindow.webContents.send('url-privanote', url);
+	} else if (url.startsWith('privanote://google-drive/auth')) {
+		const authorizationCode =
+			urlObject.searchParams.get('authorizationCode');
+
+		const tokens = await getToken(authorizationCode!);
+		setGoogleAuth(tokens);
+		initializeGoogleDrive();
+
+		const {
+			access_token: accessToken,
+			id_token: idToken,
+			refresh_token: refreshToken
+		} = tokens;
+
+		// Refresh token is only received when google account first
+		// authorizes PrivaNote so remove access here:
+		// https://myaccount.google.com/permissions
+		mainWindow.webContents.send(
+			'googleDriveAuth',
+			accessToken,
+			refreshToken,
+			idToken
+		);
+	}
+};
+
+if (!gotTheLock) {
+	app.quit();
+} else {
+	app.on('second-instance', (_, commandLine, ___) => {
+		let mainWindow = getMainWindow();
+		if (mainWindow) {
+			if (mainWindow.isMinimized()) mainWindow.restore();
+			mainWindow.focus();
+			console.log('hi');
+		}
+		if (process.platform === 'win32') {
+			console.log(commandLine);
+			deepLinkingUrl = commandLine.slice(-1)[0];
+		}
+		console.log(deepLinkingUrl);
+		handleOpenURL(deepLinkingUrl);
+	});
 }
 
 app.on('will-finish-launching', () => {
 	app.on('open-url', async (event, url) => {
 		event.preventDefault();
-
-		const urlObject = new URL(url);
-		const mainWindow = getMainWindow();
-
-		if (url.startsWith('privanote://reset-password')) {
-			mainWindow.webContents.send('url-privanote', url);
-		} else if (url.startsWith('privanote://google-drive/auth')) {
-			const authorizationCode =
-				urlObject.searchParams.get('authorizationCode');
-
-			const tokens = await getToken(authorizationCode!);
-			setGoogleAuth(tokens);
-			initializeGoogleDrive();
-
-			const {
-				access_token: accessToken,
-				id_token: idToken,
-				refresh_token: refreshToken
-			} = tokens;
-
-			// Refresh token is only received when google account first
-			// authorizes PrivaNote so remove access here:
-			// https://myaccount.google.com/permissions
-			mainWindow.webContents.send(
-				'googleDriveAuth',
-				accessToken,
-				refreshToken,
-				idToken
-			);
-		}
-
 		deepLinkingUrl = url;
+		handleOpenURL(deepLinkingUrl);
 	});
 });
 
--- client/src/shared/utils/synchronization/detectStructureChanges.ts
+++ client/src/shared/utils/synchronization/detectStructureChanges.ts
@@ -2,5 +2,9 @@ import { getNotebookStructure } from './getNotebookStructure';
 
 export const detectStructureChanges = (otherStructure: any) => {
 	let currentStructure = getNotebookStructure();
-	let changes = [];
+	let changes: any = [];
+
+	console.log(changes);
+	console.log(currentStructure);
+	console.log(otherStructure);
 };
