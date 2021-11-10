4cace557e2f9174ab746196832b319de11a9b17a
Jason Xie <thejasonxie@gmail.com>
2021-11-10 06:50:27 -0500

    Completed structure loop for detector before comparison

--- client/src/shared/utils/synchronization/detectStructureChanges.ts
+++ client/src/shared/utils/synchronization/detectStructureChanges.ts
@@ -2,15 +2,70 @@ import { getNotebookStructure } from './getNotebookStructure';
 
 let changes: any = [];
 
-const scanAndCompare = (currentStructure: any, otherStructure: any) => {
-	console.log(currentStructure);
-	console.log(otherStructure);
+const retrieveStructureDate = (structure: any) => {
+	if (structure.mimeType === 'Notebook' || structure.mimeType === 'Folder') {
+		for (let folder of structure.subFolder) {
+			if (folder.name === '.privanote') {
+				for (let file of folder.subFolder) {
+					if (file.name === 'notebookStructure.json') {
+						return file.lastModified;
+					}
+				}
+			}
+		}
+	}
 };
 
-export const detectStructureChanges = (otherStructure: any) => {
+// 1. check if new files are added
+// 2. check if files have been deleted
+
+// const comparator = (baseStructure: any, compareStructure: any) => {
+const comparator = (baseStructure: any, compareStructure: any) => {
+	let item = {
+		ids: baseStructure.ids,
+		name: baseStructure.name,
+		mimeType: baseStructure.mimeType,
+		absolutePath: baseStructure.absolutePath,
+		size: baseStructure.size,
+		dateCreated: baseStructure.dateCreated,
+		lastModified: baseStructure.lastModified,
+		statusModified: baseStructure.statusModified
+	};
+
+	if (baseStructure.subFolder.length != compareStructure.subFolder.length) {
+		// item has been either deleted or added
+	} else {
+		// loop and check that each item is exactly the same
+		// if each item is not the same, the an item has
+		// been deleted and a new one has been created
+	}
+
+	console.log(item);
+	if (item.mimeType === 'Notebook' || item.mimeType === 'Folder') {
+		for (let file of baseStructure.subFolder) {
+			console.log(file);
+			// comparator(file);
+		}
+	}
+};
+
+const scanAndCompare = (currentStructure: any, cloudStructure: any) => {
+	let currentDate = new Date(retrieveStructureDate(currentStructure));
+	let cloudDate = new Date(retrieveStructureDate(cloudStructure));
+	if (currentDate > cloudDate) {
+		// comparator(currentStructure, cloudStructure);
+		// upstream
+	} else if (cloudDate > currentDate) {
+		// comparator(cloudStructure, currentStructure);
+		// downstream
+	}
+	comparator(currentStructure, cloudStructure);
+};
+
+export const detectStructureChanges = (cloudStructure: any) => {
 	let currentStructure = getNotebookStructure();
 
-	scanAndCompare(currentStructure, otherStructure);
+	scanAndCompare(currentStructure, cloudStructure);
 
 	console.log(changes);
 };
