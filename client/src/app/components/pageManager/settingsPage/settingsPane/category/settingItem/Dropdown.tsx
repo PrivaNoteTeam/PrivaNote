508a210d54ceaaa71f9bfa1e16fa7106947bb468
Olivier Goulet <olivier_goulet@icloud.com>
2021-11-11 15:27:00 -0500

    Add selection to dropdown

--- client/src/app/components/pageManager/settingsPage/settingsPane/category/settingItem/Dropdown.tsx
+++ client/src/app/components/pageManager/settingsPage/settingsPane/category/settingItem/Dropdown.tsx
@@ -6,11 +6,24 @@ interface Props {
 	initialValue?: string;
 }
 
-export function Dropdown({ items = [], initialValue = 'Active Item' }: Props) {
+export function Dropdown({ items = [], initialValue }: Props) {
+	if (!initialValue) initialValue = items[0];
+
 	const [opened, setOpened] = useState(false);
+	const [value, setValue] = useState(initialValue);
 
 	const toggleOpen = () => {
 		setOpened(!opened);
+	};
+
+	const selectItem = (event: React.MouseEvent<HTMLLIElement>) => {
+		let selectedItem = (event.target as HTMLElement).textContent;
+
+		if (selectedItem && value !== selectedItem) {
+			setValue(selectedItem);
+		}
+
+		setOpened(false);
 	};
 
 	// testing items
@@ -22,14 +35,17 @@ export function Dropdown({ items = [], initialValue = 'Active Item' }: Props) {
 				className='pn-input flex justify-between pr-1 w-60 cursor-pointer'
 				onClick={toggleOpen}
 			>
-				<p className='text-white'>{initialValue}</p>
+				<p className='text-white'>{value}</p>
 				<ChevronDownIcon fill='#FFF' />
 			</div>
 			{opened && (
 				<div className='absolute w-60 mt-2'>
 					<ul className='bg-gray-900 border border-gray-700 rounded-md'>
 						{items.map((item) => (
-							<li className='text-white px-2 py-1 hover:bg-blue-600 cursor-pointer select-none'>
+							<li
+								onClick={selectItem}
+								className='text-white px-2 py-1 hover:bg-blue-600 cursor-pointer select-none'
+							>
 								{item}
 							</li>
 						))}
