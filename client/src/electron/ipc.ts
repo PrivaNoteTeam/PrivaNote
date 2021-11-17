286e22aceb3878f900daa871f7e66ee943636238
Jason Xie <thejasonxie@gmail.com>
2021-11-17 12:34:27 -0500

    Refactored saveItemToStructure

--- client/src/electron/ipc.ts
+++ client/src/electron/ipc.ts
@@ -8,7 +8,7 @@ import { setNotebook } from '@shared/notebook';
 import { addItemToStructure } from '@shared/utils/synchronization/addItemToStructure';
 import { deleteItemFromStructure } from '@shared/utils/synchronization/deleteItemFromStructure';
 import { renameItemInStructure } from '@shared/utils/synchronization/renameItemInStructure';
-// import { saveItemToStructure } from '@shared/utils/synchronization/saveItemToStructure';
+import { saveItemToStructure } from '@shared/utils/synchronization/saveItemToStructure';
 // import { syncUpstream } from '@shared/utils/synchronization/syncUpstream';
 import { exportNotebookStructure } from '@shared/utils/synchronization/exportNotebookStructure';
 
@@ -81,9 +81,9 @@ export function registerIpcHandlers() {
 		});
 	});
 
-	// ipcMain.on('saveFile', (_, path) => {
-	// 	saveItemToStructure(path).then((res: any) => {
-	// 		syncUpstream(res.action, res.content);
-	// 	});
-	// });
+	ipcMain.on('saveFile', (_, path) => {
+		saveItemToStructure(path).then(() => {
+			// syncUpstream(res.action, res.content);
+		});
+	});
 }
--- client/src/shared/utils/synchronization/saveItemToStructure.ts
+++ client/src/shared/utils/synchronization/saveItemToStructure.ts
@@ -1,82 +1,19 @@
-import {
-	getNotebookLocation,
-	getNotebookName,
-	getNotebookParentLocation
-} from '@shared/notebook';
-import fs from 'fs';
-import p from 'path';
 import { exportNotebookStructure } from './exportNotebookStructure';
 import { getNotebookStructure } from './getNotebookStructure';
+import { updateLastModified } from './updateLastModified';
 
-let folderChain: Array<string>;
-let itemName: string;
-let notebookName: string;
-let itemPath: string;
-let notebookPath: string;
-let level: number;
-let savedItem: {};
-
-const setupItemVariables = (item: string) => {
-	notebookPath = getNotebookLocation();
-	notebookName = getNotebookName();
-
-	itemPath =
-		item.slice(-1) === p.sep ? item.substr(0, item.length - 1) : item;
-	folderChain = itemPath.split(p.sep);
-	folderChain = folderChain.slice(folderChain.indexOf(notebookName));
-
-	itemName = folderChain.pop()!;
-
-	level = 0;
-};
-
-const saveToStructure = (structure: any) => {
-	if (!structure) return;
-	level++;
-
-	let absolutePath = p.join(getNotebookParentLocation(), ...structure.paths);
-	let parentStats = fs.statSync(absolutePath);
-	structure.size = parentStats.size;
-	structure.lastModified = parentStats.mtime;
-	structure.statusModified = parentStats.ctime;
-
-	if (level < folderChain.length) {
-		for (let folder of structure.subFolder) {
-			if (
-				folder.name === folderChain[level] &&
-				(folder.mimeType === 'Folder' || folder.mimeType === 'Notebook')
-			) {
-				saveToStructure(folder);
-			}
-		}
-	} else if (level === folderChain.length) {
-		if (
-			structure.mimeType === 'Notebook' ||
-			structure.mimeType === 'Folder'
-		) {
-			let stats = fs.statSync(itemPath);
-			let itemIndex = structure.subFolder.findIndex((item: any) => {
-				return (
-					item.name === itemName &&
-					itemPath.substr(itemPath.indexOf(notebookName)) ===
-						p.join(...item.paths)
-				);
-			});
-			structure.subFolder[itemIndex].size = stats.size;
-			structure.subFolder[itemIndex].lastModified = stats.mtime;
-			savedItem = structure.subFolder[itemIndex];
-		}
-	}
-};
-
-export const saveItemToStructure = (item: string) => {
-	return new Promise<{}>((resolve, _) => {
+/** Updates the notebook item last modified date in the notebook structure.
+ * 	@param path The absolute or relative path of the notebook item location.
+ */
+export const saveItemToStructure = (path: string) => {
+	return new Promise<{}>((_, __) => {
 		try {
-			setupItemVariables(item);
-			let notebookStructure = getNotebookStructure(notebookPath);
-			saveToStructure(notebookStructure);
+			let notebookStructure = getNotebookStructure();
+
+			notebookStructure = updateLastModified(notebookStructure, path);
+
 			exportNotebookStructure(notebookStructure);
-			resolve({ action: 'UPDATE', content: { item: savedItem } });
+			// resolve({ action: 'UPDATE', content: { item: savedItem } });
 		} catch (err) {
 			console.log(err);
 		}
